d108a6dd0419c1b438fd3e52b5fdedf66f93fa21
Alexander Hanhikoski <alexander.hanhikoski@gmail.com>
2011-11-15 08:31:13 +0200

    clean up

--- mask.rb
+++ mask.rb
@@ -7,20 +7,28 @@ def output(s)
   print s
 end
 
+# todo: would storing start-end index pairs be faster instead of a masks hash?
 class Masker
   
   def initialize()
+    @digits = "0123456789"
+    @cc_chars = "0123456789 -"
+    
     reset
   end
-  
+    
   def store_char(c)
-    if "0123456789".include? c
+    return false unless @cc_chars.include? c
+    
+    if @digits.include? c
       @digit_count += 1
       @digit_indices << @current_index
     end
 
     @current_index += 1
     @chars += c
+    
+    return true
   end
 
   def reset
@@ -29,25 +37,31 @@ class Masker
     @current_index = 0
     @chars = ""
   end
-
-  # would storing start-end index pairs be faster instead of a masks hash?
-  def print_solution    
+  
+  # prints out the string it's containing to STDOUT while masking out credit cards it finds
+  def mask
     # can't contain a valid credit card number, so return immediately
     if @digit_count < 14
       output @chars
+      reset
       return
     end
 
-    masks = {} # holds indexes of characters which should be masked
-    max_cc_length = [@digit_count, 16].min
+    # holds indexes which should be masked in the 'chars' string
+    masks = {}
+    
+    # holds the maximum credit card length the string can contain
+    # ie. if we know that a string has only 15 digits, we shouldn't check for 16 digit credit cards
+    max_cc_length = [@digit_count, 16].min 
   
+    # check credit cards with different lengths, starting from 14
     for cc_length in 14..max_cc_length do
       start_index = 0
       end_index = cc_length - 1
-    
-      while end_index < @digit_count do
       
-        if is_cc?(start_index, end_index)
+      # use brute-force to test out all possibilities
+      while end_index < @digit_count do
+        if credit_card?(start_index, end_index)
           for i in start_index..end_index do
             masks[@digit_indices[i]] = true
           end
@@ -57,17 +71,20 @@ class Masker
         end_index += 1
       end
     end
-  
-    for i in 0..(@chars.length-1) do
+    
+    # print out the solution
+    for i in 0..(@chars.length - 1) do
       if masks.has_key?(i)
         output 'X'
       else
         output @chars[i]
       end
     end
+    
+    reset
   end
 
-  def is_cc?(start_index, end_index)
+  def credit_card?(start_index, end_index)
     odd = true
     sum = 0
     
@@ -82,21 +99,16 @@ class Masker
   end
 end
 
-cc_chars = "0123456789 -"
 masker = Masker.new
-
 line_feeds = 0
 begin
   c = $stdin.getc
   
-  if cc_chars.include? c
-    masker.store_char(c)
+  if masker.store_char(c)
     next
   end
     
-  masker.print_solution
-  masker.reset
-  
+  masker.mask
   output c
   
   if c == "\n"
